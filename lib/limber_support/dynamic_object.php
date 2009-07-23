7a27e9fae28a473f262e508287d5f4e05ce73de9
Wilker Lucio <wilkerlucio@gmail.com>
2009-07-23 00:53:04 -0300

    created DynamicObject class

---
+++ lib/limber_support/dynamic_object.php
@@ -0,0 +1,344 @@
+<?php
+
+/*
+ * Copyright 2009 Limber Framework
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+namespace LimberSupport;
+
+/**
+ * DynamicObject provides some meta-programming features in a hand
+ *
+ * Many times you need to modify the classes outside of class definitions,
+ * or simple to write code thats write code. If you think in this a good,
+ * this class is for you.
+ *
+ * The simpler way to describe the DynamicObject, is to say that this class
+ * provides a way to you modifify the classes outside of their definition. To
+ * give this capacity to the classe you only need to extend from DynamicObject
+ * and them the DynamicObject will do all the magic for you.
+ *
+ * At first DynamicObject provides a default extensions for you, let's see
+ * these default extensions first.
+ *
+ * Using property accessors:
+ *
+ * Many times you follow that OOP pattern: define a private variable, define
+ * the getter method, define the setter method, like in the code above
+ *
+ * <code>
+ * class Person
+ * {
+ *    private $name;
+ *
+ *    public function get_name()
+ *    {
+ *      return $this->name;
+ *    }
+ *
+ *    public function set_name($name)
+ *    {
+ *      $this->name = $name;
+ *    }
+ * }
+ * </code>
+ *
+ * This kind of code is repeated in almost all of your code, its big and
+ * exaustive.
+ *
+ * With the meta-programming of DynamicObject you can do the same think with
+ * the following code
+ *
+ * <code>
+ * class Person extends LimberSupport\DynamicObject
+ * {
+ *	 public $_name;
+ * }
+ *
+ * Person::attr_accessor("name");
+ *
+ * $p = new Person();
+ * $p->name = "Foo"
+ * 
+ * echo $p->name; // => "Foo"
+ * </code>
+ *
+ * And later, when you need to put some validation or modification of attribute
+ * you can do this simple
+ *
+ * <code>
+ * class Person extends LimberSupport\DynamicObject
+ * {
+ *   public $_name;
+ *
+ *   public function set_name($name)
+ *   {
+ *	   if (strlen($name) > 3) $this->_name = $name;
+ *   }
+ * }
+ * Person::attr_accessor("name");
+ *
+ * $p = new Person();
+ * $p->name = "Foo"
+ * 
+ * echo $p->name; // => "Foo"
+ * </code>
+ * 
+ * As you notice, the variable needs to be public, the reason is the variable
+ * will be externally accessed. This breaks the encapsulation a little, yes,
+ * but if it's private, since PHP 5.3 you can access this by reflection (even
+ * it's private). The point here is the DynamicObject is creating the methods
+ * outside of the class, this can be usefull when you need a class that will
+ * needs to be external extended.
+ *
+ * Now let's see how to create your own extensions
+ *
+ * //TODO: doc for create extensions
+ *
+ * @package LimberSupport
+ */
+abstract class DynamicObject
+{
+	public $_instance_methods = array();
+	public $_dynamic_attributes = array();
+	
+	public function define_instance_method($method_name, $callback)
+	{
+		$this->_instance_methods[$method_name] = $callback;
+	}
+	
+	public static function define_getter($callback)
+	{
+		\ClassParams::append(get_called_class(), "getters", $callback);
+	}
+	
+	public static function define_setter($callback)
+	{
+		\ClassParams::append(get_called_class(), "setters", $callback);
+	}
+	
+	public static function define_method($method_name, $callback)
+	{
+		$methods = \ClassParams::get(get_called_class(), "methods", array());
+		$methods[$method_name] = $callback;
+		
+		\ClassParams::set(get_called_class(), "methods", $methods);
+	}
+	
+	public static function define_static_method($method_name, $callback)
+	{
+		$methods = \ClassParams::get(get_called_class(), "static_methods", array());
+		$methods[$method_name] = $callback;
+		
+		\ClassParams::set(get_called_class(), "static_methods", $methods);
+	}
+	
+	public function has_method($method_name)
+	{
+		if (method_exists($this, $method_name)) {
+			return true;
+		}
+		
+		if (isset($this->_instance_methods[$method_name])) {
+			return true;
+		}
+		
+		$iterator = new ClassAncestorsIterator(get_class($this));
+		
+		foreach ($iterator as $current_class) {
+			$methods = \ClassParams::get($current_class, "methods", array());
+			
+			if (isset($methods[$method_name])) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public function _get_property($var)
+	{
+		$method_name = "get_{$var}";
+		
+		if ($this->has_method($method_name)) {
+			return $this->$method_name();
+		}
+		
+		throw new CallerContinueException();
+	}
+	
+	public function _get_function($var)
+	{
+		if ($this->has_method($var)) {
+			return $this->$var();
+		}
+		
+		throw new CallerContinueException();
+	}
+	
+	public function _set_property($var, $value)
+	{
+		$method_name = "set_{$var}";
+		
+		if ($this->has_method($method_name)) {
+			return $this->$method_name($value);
+		}
+		
+		throw new CallerContinueException();
+	}
+	
+	public function __get($var)
+	{
+		$iterator = new ClassAncestorsIterator(get_class($this));
+		
+		foreach ($iterator as $current_class) {
+			$getters = array_reverse(\ClassParams::get($current_class, "getters", array()));
+			
+			foreach ($getters as $getter) {
+				try {
+					if (is_string($getter)) {
+						$value = $this->$getter($var);
+					} else {
+						$value = $getter($this, $var);
+					}
+				
+					return $value;
+				} catch (CallerContinueException $e) {}
+			}
+		}
+		
+		throw new CallerNotFoundException("Can't handle getter for {$var}");
+	}
+	
+	public function __set($var, $value)
+	{
+		$iterator = new ClassAncestorsIterator(get_class($this));
+		
+		foreach ($iterator as $current_class) {
+			$setters = array_reverse(\ClassParams::get($current_class, "setters", array()));
+			
+			foreach ($setters as $setter) {
+				try {
+					if (is_string($setter)) {
+						$value = $this->$setter($var, $value);
+					} else {
+						$value = $setter($this, $var, $value);
+					}
+				
+					return $value;
+				} catch (CallerContinueException $e) {}
+			}
+		}
+		
+		throw new CallerNotFoundException("Can't handle setter for {$var}");
+	}
+	
+	public function __call($method, $arguments)
+	{
+		$iterator = new ClassAncestorsIterator(get_class($this));
+		
+		array_unshift($arguments, $this);
+		
+		if (isset($this->_instance_methods[$method])) {
+			try {
+				$value = call_user_func_array($this->_instance_methods[$method], $arguments);
+				return $value;
+			} catch(CallerContinueException $e) {}
+		}
+		
+		foreach ($iterator as $current_class) {
+			$methods = \ClassParams::get($current_class, "methods", array());
+			
+			if (isset($methods[$method])) {
+				try {
+					$value = call_user_func_array($methods[$method], $arguments);
+					return $value;
+				} catch(CallerContinueException $e) {}
+			}
+		}
+		
+		throw new CallerNotFoundException("Can't handle caller for {$method}");
+	}
+	
+	public static function __callStatic($method, $arguments)
+	{
+		$iterator = new ClassAncestorsIterator(get_called_class());
+		
+		array_unshift($arguments, get_called_class());
+		
+		foreach ($iterator as $current_class) {
+			$methods = \ClassParams::get($current_class, "static_methods", array());
+			
+			if (isset($methods[$method])) {
+				try {
+					$value = call_user_func_array($methods[$method], $arguments);
+					return $value;
+				} catch(CallerContinueException $e) {}
+			}
+		}
+		
+		throw new CallerNotFoundException("Can't handle static caller for {$method}");
+	}
+}
+
+//basic getters and setters
+DynamicObject::define_getter("_get_function");
+DynamicObject::define_getter("_get_property");
+DynamicObject::define_setter("_set_property");
+
+//defining attribute accessors
+DynamicObject::define_static_method("attr_reader", function() {
+	$attributes = func_get_args();
+	$class = array_shift($attributes);
+	
+	foreach ($attributes as $attr) {
+		$class::define_method("get_{$attr}", function($object) use ($attr) {
+			$attr = "_" . $attr;
+			return $object->$attr;
+		});
+	}
+});
+
+DynamicObject::define_static_method("attr_writer", function() {
+	$attributes = func_get_args();
+	$class = array_shift($attributes);
+		
+	foreach ($attributes as $attr) {
+		$class::define_method("set_{$attr}", function($object, $value) use ($attr) {
+			$attr = "_" . $attr;
+			$object->$attr = $value;
+		});
+	}
+});
+
+DynamicObject::define_static_method("attr_accessor", function() {
+	$attributes = func_get_args();
+	$class = array_shift($attributes);
+	
+	foreach ($attributes as $attr) {
+		$class::define_method("get_{$attr}", function($object) use ($attr) {
+			$attr = "_" . $attr;
+			return $object->$attr;
+		});
+		
+		$class::define_method("set_{$attr}", function($object, $value) use ($attr) {
+			$attr = "_" . $attr;
+			$object->$attr = $value;
+		});
+	}
+});
+
+class CallerContinueException extends \Exception {}
+class CallerNotFoundException extends \Exception {}
---
+++ spec/support/dynamic_object_spec.php
@@ -0,0 +1,260 @@
+<?php
+
+/*
+ * Copyright 2009 Limber Framework
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+namespace LimberSupport;
+
+require_once "limber_support.php";
+
+class DynamicObjectTest extends DynamicObject
+{
+	public $n = 0;
+	public $_variable;
+	
+	public function simple()
+	{
+		return "simple";
+	}
+	
+	public function get_item()
+	{
+		return "item value";
+	}
+	
+	public function internal($var)
+	{
+		if ($var == "internal_getter") {
+			return "into scope";
+		}
+		
+		throw new CallerContinueException();
+	}
+	
+	public function internal_setter($var, $value)
+	{
+		if ($var == "number") {
+			$this->n += $value;
+			return;
+		}
+		
+		throw new CallerContinueException();
+	}
+	
+	public function set_numeric($value)
+	{
+		$this->n += $value;
+	}
+}
+
+DynamicObjectTest::attr_accessor("variable");
+
+DynamicObjectTest::define_getter("internal");
+DynamicObjectTest::define_getter(function($object, $var) {
+	if ($var == "lambda_getter") {
+		return "lambda rocks!";
+	}
+	
+	throw new CallerContinueException();
+});
+
+DynamicObjectTest::define_setter("internal_setter");
+DynamicObjectTest::define_setter(function($object, $var, $value) {
+	if ($var == "num") {
+		$object->n += $value;
+		return;
+	}
+	
+	throw new CallerContinueException();
+});
+
+DynamicObjectTest::define_method("cool_think", function($object, $arg = "") {
+	return $arg . $object->simple();
+});
+
+DynamicObjectTest::define_static_method("some_static", function($class_name, $arg = "") {
+	return $arg . $class_name;
+});
+
+DynamicObjectTest::define_static_method("define_simple_getter", function($class_name, $getter) {
+	$class_name::define_method($getter, function($object) use ($getter) {
+		return $getter;
+	});
+});
+
+describe("Dynamic Object", function($spec) {
+	$spec->context("using attr_accessors", function($spec) {
+		$spec->it("should use dynamic attribute", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			$obj->variable = 10;
+			
+			$spec($obj->variable)->should->be(10);
+		});
+	});
+	
+	$spec->context("calling dynamic getters", function($spec) {
+		$spec->context("user getters", function($spec) {
+			$spec->it("should get a lambda like getter", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$spec($obj->lambda_getter)->should->be("lambda rocks!");
+			});
+			
+			$spec->it("should get a internal function", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$spec($obj->internal_getter)->should->be("into scope");
+			});
+		});
+		
+		$spec->context("default getters", function($spec) {
+			$spec->it("should call return the value if the class has a get_x method", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$spec($obj->item)->should->be("item value");
+			});
+			
+			$spec->it("should call a simple function if its present", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$spec($obj->simple)->should->be("simple");
+			});
+			
+			$spec->it("should call a simple function if its present as dynamic method", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$spec($obj->cool_think)->should->be("simple");
+			});
+		});
+		
+		$spec->it("should throw an CallerNotFoundException when dont find a getter", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			
+			try {
+				$obj->it_doesnt_exists;
+				$spec(true)->should->be(false);
+			} catch(CallerNotFoundException $e) {
+				$spec(true)->should->be(true);
+			}
+		});
+	});
+	
+	$spec->context("calling dynamic setters", function($spec) {
+		$spec->context("calling user setters", function($spec) {
+			$spec->it("should works for internal setters", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$obj->number = 3;
+				$obj->number = 5;
+				
+				$spec($obj->n)->should->be(8);
+			});
+			
+			$spec->it("should works for lambda setters", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$obj->num = 3;
+				$obj->num = 1;
+				
+				$spec($obj->n)->should->be(4);
+			});
+		});
+		
+		$spec->context("calling default setters", function($spec) {
+			$spec->it("should call set_x operation when present", function($spec, $data) {
+				$obj = new DynamicObjectTest();
+				$obj->numeric = 3;
+				$obj->numeric = 4;
+				
+				$spec($obj->n)->should->be(7);
+			});
+		});
+		
+		$spec->it("should throw an CallerNotFoundException when dont find a setter", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			
+			try {
+				$obj->it_doesnt_exists = 4;
+				$spec(true)->should->be(false);
+			} catch(CallerNotFoundException $e) {
+				$spec(true)->should->be(true);
+			}
+		});
+	});
+	
+	$spec->context("calling instance methods", function($spec) {
+		$spec->it("should call the user defined method", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			$obj->define_instance_method("hello", function() {
+				return "world";
+			});
+			
+			$spec($obj->hello())->should->be("world");
+		});
+		
+		$spec->it("should override normal dynamic methods", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			$obj->define_instance_method("cool_think", function() {
+				return "world";
+			});
+			
+			$spec($obj->cool_think())->should->be("world");
+		});
+	});
+	
+	$spec->context("calling dynamic methods", function($spec) {
+		$spec->it("should call a dynamic method", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			$spec($obj->cool_think())->should->be("simple");
+		});
+		
+		$spec->it("should accept arguments", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			$spec($obj->cool_think("hello "))->should->be("hello simple");
+		});
+		
+		$spec->it("should be callable by call_user_func_array", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			$spec(call_user_func_array(array($obj, "cool_think"), array("hello ")))->should->be("hello simple");
+		});
+		
+		$spec->it("should throw an CallerNotFoundException when dont find a method", function($spec, $data) {
+			$obj = new DynamicObjectTest();
+			
+			try {
+				$obj->it_doesnt_exists();
+				$spec(true)->should->be(false);
+			} catch(CallerNotFoundException $e) {
+				$spec(true)->should->be(true);
+			}
+		});
+	});
+	
+	$spec->context("calling dynamic static methods", function($spec) {
+		$spec->it("should call a dynamic static method", function($spec, $data) {
+			$spec(DynamicObjectTest::some_static())->should->be("LimberSupport\\DynamicObjectTest");
+		});
+		
+		$spec->it("should can define some methods inside", function($spec, $data) {
+			DynamicObjectTest::define_simple_getter("limber_static");
+			
+			$obj = new DynamicObjectTest();
+			$spec($obj->limber_static)->should->be("limber_static");
+		});
+		
+		$spec->it("should throw an CallerNotFoundException when dont find a static method", function($spec, $data) {
+			try {
+				DynamicObjectTest::it_doesnt_exists();
+				$spec(true)->should->be(false);
+			} catch(CallerNotFoundException $e) {
+				$spec(true)->should->be(true);
+			}
+		});
+	});
+});
